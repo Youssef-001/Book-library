8f4655b4d613d7eb221312cb08041c91ae6f9e38
Youssef-001 <youssefsamir432@gmail.com>
2024-07-15 18:41:12 +0300

    Finish page stylings Add delete button, close form button

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const myLibrary = [];
 
 
-function Book(title, author, numberPages,read, cover)
+function Book(title, author, numberPages,read, description, cover)
 {
     this.title = title;
     this.author = author;
     this.numberPages = numberPages;
     this.read = read;
+    this.description = description;
     this.cover = cover;
 
 }
@@ -36,6 +37,8 @@ let cover = document.querySelector("#cover");
 
 let form = document.querySelector("form");
 
+let description = document.querySelector("#description");
+
 let pageContent = document.querySelector(".page-content");
 
 
@@ -49,39 +52,79 @@ submit.addEventListener('click', function(e){
     let image = val[2];
     console.log(val[2]);
     // let newBook = new Book(title.value, author.value, numberPages.value, read.checked, "image");
-    let newBook = new Book(title.value, author.value, numberPages.value, read.checked, image);
+    let newBook = new Book(title.value, author.value, numberPages.value, read.checked,description.value, image);
     addBookToLibrary(newBook);
     // console.log(myLibrary)
     form.classList.add('hidden');
+    pageContent.style.opacity="1";
 
 
 
     let card = document.createElement('div');
     card.classList.add('card')
+    let imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
+
     let card_img = document.createElement('img');
     card_img.src = `./images/${image}`
-    card.appendChild(card_img);
+    imageContainer.appendChild(card_img);
+    card.appendChild(imageContainer);
 
     let cardContent = document.createElement('div');
+    cardContent.classList.add('card-content');
     let cardTitle = document.createElement('div');
+    cardTitle.classList.add("book-title")
     cardTitle.textContent = title.value;
     cardContent.appendChild(cardTitle);
 
     let cardAuthor = document.createElement('div');
     cardContent.appendChild(cardAuthor);
-    cardAuthor.textContent = `by ${author.value}`;
+    cardAuthor.classList.add("book-author")
+    cardAuthor.textContent = `By `;
+    let cardAuthorSpan = document.createElement('span');
+    cardAuthorSpan.textContent = author.value;
+    cardAuthor.appendChild(cardAuthorSpan);
+
+    let cardDescription = document.createElement('div');
+    cardDescription.classList.add('book-description');
+    cardDescription.textContent = description.value;
+    cardContent.appendChild(cardDescription);
 
     let cardPages = document.createElement('div');
     cardContent.appendChild(cardPages);
+    cardPages.classList.add("book-pages")
     cardPages.textContent = `Pages: ${numberPages.value}`;
 
     let cardRead = document.createElement('div');
     cardContent.appendChild(cardRead);
+    cardRead.classList.add("book-read")
     cardRead.textContent = `Read: ${read.checked? 'Yes' : 'No'}`;
+
+
+    let buttonsDiv = document.createElement('div');
+
+
 
     let changeStatus = document.createElement('button');
     changeStatus.textContent = 'Read? ';;
-    cardContent.appendChild(changeStatus);
+    changeStatus.addEventListener('click', function(){
+        newBook.read =!newBook.read;
+        cardRead.textContent = `Read: ${newBook.read? 'Yes' : 'No'}`;
+    })
+    buttonsDiv.appendChild(changeStatus);
+
+
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    
+    buttonsDiv.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', function(){
+        myLibrary.splice(myLibrary.indexOf(newBook), 1);
+        card.remove();
+    })
+
+    cardContent.appendChild(buttonsDiv);
 
     card.appendChild(cardContent);
     pageContent.appendChild(card);
@@ -96,4 +139,13 @@ let addButton = document.querySelector(".btn");
 addButton.addEventListener('click', function(e) {
 
    form.classList.remove('hidden'); 
+   pageContent.style.opacity="0.1"
+} )
+
+
+let xButton = document.querySelector(".x-button");
+
+xButton.addEventListener('click', function(e) {
+    form.classList.add('hidden');
+    pageContent.style.opacity="1";
 } )
